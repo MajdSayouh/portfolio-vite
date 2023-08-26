1085021bf4dfde8bee70af5d3006b7cd50f7c272
MajdSayouh <ali.abdo.laith@gmail.com>
2023-08-25 16:30:58 -0800

    first commit

---
+++ src/data/user.js
@@ -0,0 +1,102 @@
+import reactLogo from "../assets/R (1).png";
+import Logo from "../assets/logo.png";
+import about from "../assets/about.jpg";
+
+const INFO = {
+	main: {
+		title: "Reactfolio by truethari",
+		name: "Majd Sayouh.",
+		email: "majdsayouh222@gmail.com",
+		logo: Logo
+	},
+
+	socials: {
+		twitter:
+			"https://twitter.com/Majd_Sayouh?t=4-JdzeqVnTHqGqV-wASWng&s=09",
+		github: "https://github.com/MajdSayouh?tab=repositories",
+		linkedin: "https://www.linkedin.com/in/majd-sayouh-81008b218/",
+		instagram:
+			"https://instagram.com/majd_sayouh?utm_source=qr&igshid=NGExMmI2YTkyZg%3D%3D",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://www.facebook.com/majd.sayouh?mibextid=D4KYlr"
+	},
+
+	homepage: {
+		title: "Software Engineer and Front End developer.",
+		description: `I am a junior front-end developer and enthusiast web applications using HTML, CSS, javascript and Reactjs. I am passionate about learning and understanding web technologies in depth.
+			In addition, I am an extremely organized person who is focused on producing results, while i am always realistic when setting goals, i consistently develop ways to efficiently achieve and often exceed these goals.
+			And i think i am a fun-loving person with a good sense of self and positive nature, I love sports, chess, movies and reading books about competitive programming and web development.`
+	},
+
+	about: {
+		title: "I’m Majd Sayouh. I live in Syria, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback."
+	},
+
+	articles: {
+		title:
+			"I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more."
+	},
+
+	projects: [
+		{
+			title: "FocalX Task1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: reactLogo,
+			linkText: "View Project",
+			link: "https://github.com/MajdSayouh/Front_End_Adv_Task1"
+		},
+
+		{
+			title: "Reminder App",
+			description:
+				"Designed and developed a Reminder App using React JS as the front-end and Redux for state management.",
+			logo: reactLogo,
+			linkText: "View Project",
+			link: "https://github.com/MajdSayouh/reminder-app-react"
+		},
+
+		{
+			title: " Basic E-commerce",
+			description:
+				"o Developed a basic React JS app that fetches data from a fake API using Fake API Store and displays it to the user.				",
+			logo: reactLogo,
+			linkText: "View Project",
+			link: "https://github.com/MajdSayouh/Basic-E-commerce"
+		},
+
+		{
+			title: " E-State-Design",
+			description:
+				"o Developed a responsive web application using React JS, and React Router to improve the user experience.",
+			logo: reactLogo,
+			linkText: "View Project",
+			link: "https://github.com/MajdSayouh/e_State_Design_reactjs"
+		},
+
+		{
+			title: "Html_Css_Template1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo:
+				"https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com/MajdSayouh/Html_Css_Template1"
+		},
+		{
+			title: "Search App",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo:
+				"https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/MajdSayouh/searchApp-react"
+		}
+	]
+};
+
+export default INFO;
